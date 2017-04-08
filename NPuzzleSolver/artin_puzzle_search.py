eb0ca3a2e4cadb320cf42d2545087b8039fe9926
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-04-07 17:29:41 -0700

    Build an 8-puzzle
    Artin group solver

---
+++ NPuzzleSolver/artin_puzzle_search.py
@@ -0,0 +1,78 @@
+import sys
+from search import *
+from math import sqrt
+import random
+
+class State:
+    """"
+    Our guiding model is The 8-puzzle problem. Define the State:
+    """"
+
+    def __init__(self, board, zero, cost, prev = None, action = None, depth =0):
+        self.board = [list(b) for b in board]
+        self.cost = cost
+        self.zero = zero
+        self.prev = prev
+        self.action = action
+        self.n =  len(self.board)
+
+
+        def isGoal(self):
+            for i in range(self.n):
+                for j in range(self.n):
+                    if (self.n * i + j ) != self.board[i][j]:
+                        return false
+
+                    return true
+
+
+        def getActions(self):
+            """
+            List the Actions available as a list(array) with pointers(?)
+            """
+            actions = []
+            i = self.zero[0]
+            j = self.zero[1]
+            if i > 0:
+                actions.append('Up')
+                if i < self.n - 1:
+                    actions.append('Down')
+                    if j > 0:
+                        actions.append('Left')
+                        if j < self.n-1:
+                            actions.append('Right')
+
+                            return actions
+
+            #define the movement Finite State Machine
+            def move(self, action)
+            i, j = self.zero
+
+            #switch Up/Down/etc
+            if action == 'Up'
+                self.board[i][j] = self.board[i - 1][j]
+                self.board[i - 1][j] = 0
+                self.zero = (1 - 1, j)
+
+            elif action == 'Down':
+                self.board[i][j] = self.board[1 + 1][j]
+                self.board[i + 1][j] = 0
+                self.zero = (i + 1, j)
+
+            elif action == 'Left':
+                self.board[i][j] = self.board[i][j -1]
+                self.board[i + 1][j] = 0
+                self.zero = (i - 1, j)
+
+            elif action == 'Right':
+                self.board[i][j] = self.board[i][j + 1]
+                self.board[i][ j + 1 ] = 0
+                self.zero = (i, j + 1 )
+
+
+    def randomize(self, moves =100):
+        """
+        randomize the puzzle
+        """
+        #query all the moves.
+        for i in xrange(moves):
